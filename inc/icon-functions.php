ad17564c8c37784d1b63d9e03affb971032c5a92
Frolov <phrolov.ivan@gmail.com>
2020-09-28 11:38:26 +0300

    #2 fix xlink:href in svg

--- inc/icon-functions.php
+++ inc/icon-functions.php
@@ -90,7 +90,7 @@ function starter_get_svg( $args = array() ) {
 	 *
 	 * See https://core.trac.wordpress.org/ticket/38387.
 	 */
-	$svg .= ' <use href="#' . esc_html( $args['icon'] ) . '" xlink:href="#icon-' . esc_html( $args['icon'] ) . '"></use> ';
+	$svg .= ' <use href="#' . esc_html( $args['icon'] ) . '" xlink:href="' . esc_html( $args['icon'] ) . '"></use> ';
 
 	// Add some markup to use as a fallback for browsers that do not support SVGs.
 	if ( $args['fallback'] ) {
