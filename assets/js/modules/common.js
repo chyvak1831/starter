e845b3ff81e7bb52d3d48feab3bff02aa15e405a
Frolov <phrolov.ivan@gmail.com>
2021-08-03 20:51:08 +0300

    css refactoring: completed

--- assets/js/modules/common.js
+++ assets/js/modules/common.js
@@ -22,7 +22,7 @@ $( window ).scroll( function() {
 
 // fix form classes
 function addFormClass() {
-	var $primary_btn = '[name="apply_coupon"], [name="update_cart"], [name="apply_coupon"], [name="save_address"], [name="tinvwl-add-to-cart"], [name="tinvwl-action"]';
+	var $primary_btn = '[name="apply_coupon"], [name="update_cart"], [name="apply_coupon"], [name="save_address"], [name="tinvwl-add-to-cart"], [name="tinvwl-action"], .woocommerce-Button';
 	var $primary_btn_lg = '.woocommerce-form-login__submit, .woocommerce-form-register__submit, .lost_reset_password .woocommerce-Button, .wc-proceed-to-checkout .button, #place_order';
 	$( '.input-text' ).addClass( 'form-control' );
 	$( 'select' ).addClass( 'form-select' );
