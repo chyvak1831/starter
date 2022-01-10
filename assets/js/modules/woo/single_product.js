b8bf1279995630b9c3257b2f215fd3b63b99841b
Frolov <phrolov.ivan@gmail.com>
2022-01-10 19:05:27 +0200

    #137

--- assets/js/modules/woo/single_product.js
+++ assets/js/modules/woo/single_product.js
@@ -1,33 +1,6 @@
 window.addEventListener( 'load', () => {
 
 
-// plus/minus product
-const plusMinusProduct = () => {
-	const btn = document.querySelectorAll( '.js_count_add_product [data-count]' );
-	btn.forEach( element => element.addEventListener( 'click', e => {
-		e.preventDefault();
-
-		const btnType = e.currentTarget.getAttribute( 'data-count' );
-		const input = e.currentTarget.closest( '.js_count_add_product' ).querySelector( '[type="number"]' );
-		let currentValue = +input.value;
-		const newValue = {}
-
-		// count new value
-		if ( 'minus' == btnType ) {
-			if ( currentValue < 2 ) return;
-			newValue.amount = --currentValue;
-		} else {
-			newValue.amount = ++currentValue;
-		}
-
-		// set new value to input and button 'add to cart'
-		input.value = newValue.amount;
-		document.querySelector( '.js_add_to_cart_btn' ).setAttribute( 'data-quantity', newValue.amount );
-	}))
-}
-plusMinusProduct();
-
-
 // zoom product page
 const zoomImage = () => {
 	const zoomWrap = document.querySelector( '.js_zoom_wrap' );
@@ -53,6 +26,33 @@ const zoomImage = () => {
 	})
 }
 zoomImage();
+
+
+// plus/minus product
+const plusMinusProduct = () => {
+	const btn = document.querySelectorAll( '.js_count_add_product [data-count]' );
+	btn.forEach( element => element.addEventListener( 'click', e => {
+		e.preventDefault();
+
+		const btnType = e.currentTarget.getAttribute( 'data-count' );
+		const input = e.currentTarget.closest( '.js_count_add_product' ).querySelector( '[type="number"]' );
+		let currentValue = +input.value;
+		const newValue = {}
+
+		// count new value
+		if ( 'minus' == btnType ) {
+			if ( currentValue < 2 ) return;
+			newValue.amount = --currentValue;
+		} else {
+			newValue.amount = ++currentValue;
+		}
+
+		// set new value to input and button 'add to cart'
+		input.value = newValue.amount;
+		document.querySelector( '.js_add_to_cart_btn' ).setAttribute( 'data-quantity', newValue.amount );
+	}))
+}
+plusMinusProduct();
 
 
 });/*end window load event*/
